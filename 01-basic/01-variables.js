8dd414d44fd508304075b5dc183993ed7769137d
Sunil Ghate <05510@sdloffice.com>
2026-06-21 08:40:26 +0530

    First lession - Let Const and Var

---
+++ 01-basic/01-variables.js
@@ -0,0 +1,18 @@
+const accountId = 12344444
+let accountName = "Sunil Ghate"
+var accountPassword = "12345"
+accountCity = "Nashik"
+let accountState;
+
+//Let console log the variables
+console.log(accountId);
+
+//Try to modify the values previously assigned
+// accountId = 3456666
+// console.log(accountId);
+
+accountName = "derfff"
+accountPassword = "frtttt"
+accountCity = "sererere"
+
+console.table([accountName, accountPassword,accountCity, accountState])
